74f66cbc31e70a47f234e24da8ea19a4592a0615
daySosa <daym_sosan50@unicah.edu>
2026-02-27 22:03:15 -0600

    Menú órdenes

---
+++ Órdenes de Trabajo/MenúPrincipalOrdenes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Órdenes_de_Trabajo
+{
+    /// <summary>
+    /// Lógica de interacción para MenúPrincipalOrdenes.xaml
+    /// </summary>
+    public partial class MenúPrincipalOrdenes : Window
+    {
+        public MenúPrincipalOrdenes()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNuevaOrden_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí abres tu ventana de nueva orden cuando la tengas
+            MainFrame.Navigate(new MainWindow());
+        }
+    }
+}
