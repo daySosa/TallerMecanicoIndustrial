1ffec661f9828507aef6754b07cb7011c6602bf5
cesia44 <cesia4469@gmail.com>
2026-03-02 21:19:39 -0600

    Menu de Ingresos

---
+++ Contabilidad/Contabilidad/MenuDePagos.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contabilidad
+{
+    /// <summary>
+    /// Lógica de interacción para MenuDePagos.xaml
+    /// </summary>
+    public partial class MenuDePagos : Window
+    {
+        public MenuDePagos()
+        {
+            InitializeComponent();
+        }
+    }
+}
