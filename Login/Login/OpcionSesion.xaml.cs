322f78b815ede5b52301b0dbac1e968ec1a47d90
valeriaperdomo6 <valeriaperdomo676@gmail.com>
2026-03-04 20:08:08 -0600

    OpcionSesion

---
+++ Login/Login/OpcionSesion.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Lógica de interacción para OpcionSesion.xaml
+    /// </summary>
+    public partial class OpcionSesion : Page
+    {
+        public OpcionSesion()
+        {
+            InitializeComponent();
+        }
+    }
+}
